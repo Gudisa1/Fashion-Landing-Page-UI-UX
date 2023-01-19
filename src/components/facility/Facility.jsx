66bfaa917384c0964f61b6ed1fc0138603ae9d6a
Gudisa Gebi <gudisagebi1@gmail.com>
2023-01-18 16:16:21 -0800

    Hero Section Complete

--- src/components/facility/Facility.jsx
+++ src/components/facility/Facility.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 import "./facility.css";
+import { BsTruck } from "react-icons/bs";
+import { FaCcApplePay } from "react-icons/fa";
+import { GiReturnArrow } from "react-icons/gi";
+import { AiFillClockCircle } from "react-icons/ai";
+
 function facility() {
-  return <div>facility</div>;
+  return (
+    <section>
+      <div className="facility-container">
+        <div className="facility">
+          <BsTruck className="facility-icon" />
+          <h3 className="facility-heading">Free Shipping</h3>
+          <p className="facility-paragraph">
+            Ut enim ad minim veniam liquip ami tomader
+          </p>
+        </div>
+        <div className="facility">
+          <FaCcApplePay className="facility-icon" />
+          <h3 className="facility-heading">Secure Payments</h3>
+          <p className="facility-paragraph">
+            Eonim ad minim veniam liquip tomader
+          </p>
+        </div>
+        <div className="facility">
+          <GiReturnArrow className="facility-icon" />
+          <h3 className="facility-heading">Easy Returns</h3>
+          <p className="facility-paragraph">
+            Be enim ad minim veniam liquipa ami tomader
+          </p>
+        </div>
+        <div className="facility">
+          <AiFillClockCircle className="facility-icon" />
+          <h3 className="facility-heading">24/7 Support</h3>
+          <p className="facility-paragraph">
+            Ut enim ad minim veniam liquip ami tomader
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default facility;
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import "./hero.css";
+import hero from "../../assets/img/hero.jpg";
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <main>
+      <div className="hero-container">
+        <div className="hero-text">
+          <h3 className="hero-title">SUMMER SALE IS ON</h3>
+          <h2 className="hero-description">Make Your Fashion More Perfect</h2>
+          <p className="hero-paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <button className="btn">
+            <a href="" class="btn-text">
+              Explore More
+            </a>
+          </button>
+        </div>
+        <div className="hero-img">
+          <img src={hero} alt="Hero Section Image" className="hero-image" />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Hero;
